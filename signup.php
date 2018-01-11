2c42251f0941c434a39ae557c4b310acc97f7a99
SCcat8822 <huntercreations888@gmail.com>
2018-01-11 22:06:41 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,3 +1,22 @@
 <?php
-include
+
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AeroFood - Sign Up</title>
+	<link rel="stylesheet" type="text/css" href="aerofood/stylesheet.css">
+</head>
+<body>
+	<main>
+			<div>
+	<form>
+<input type="text" name="username">
+<input type="text" name="email">
+<input type="password" name="password">
+</div>
+</form>
+</main>
+
+</body>
+</html>
