03e19b37c70ab0fe762154f4fbc18417c6b8b327
SCcat8822 <huntercreations888@gmail.com>
2018-01-11 21:52:49 +0000

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,4 +3,6 @@ $dbServername = "85.10.205.173:3306";
 $password = "SCcat882!";
 $username = "aerofood";
 $dbName = "aerofood";
+
+$conn = mysqli_connect($dvServername, $password, $username, $dbName);
 ?>
